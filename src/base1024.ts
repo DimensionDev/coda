668e2eab4281bba694d7b5b0359dd7ffbe900eaf
Septs <github@septs.pw>
2020-05-13 18:44:35 +0800

    chore: rename variable naming

--- src/base1024.ts
+++ src/base1024.ts
@@ -17,15 +17,15 @@ function trimRight(input: Uint8Array): Uint8Array {
  * Encode Uint8Array to base1024
  */
 export function encode(input: Uint8Array): string {
-  let output = ''
+  const points: number[] = []
   for (let i = 0; i < input.length; i += 5) {
     const alpha = ((input[i] & 0xff) << 2) | (input[i + 1] >> 6)
     const beta = ((input[i + 1] & 0x3f) << 4) | (input[i + 2] >> 4)
     const gamma = ((input[i + 2] & 0xf) << 6) | (input[i + 3] >> 2)
     const delta = ((input[i + 3] & 0x3) << 8) | input[i + 4]
-    output += [alpha, beta, gamma, delta].map((ch: number) => EMOJIS[ch]).join('')
+    points.push(alpha, beta, gamma, delta)
   }
-  return output
+  return points.map((ch) => EMOJIS[ch]).join('')
 }
 
 /**
@@ -34,15 +34,15 @@ export function encode(input: Uint8Array): string {
 export function decode(input: string): Uint8Array {
   const source: number[] = Array.from(input).map((e: string) => EMOJIS.indexOf(e))
 
-  const packet: number[] = []
+  const points: number[] = []
   for (let i = 0; i < input.length; i += 4) {
     const alpha = source[i] >> 2
     const beta = ((source[i] & 0x3) << 6) | (source[i + 1] >> 4)
     const gamma = ((source[i + 1] & 0xf) << 4) | (source[i + 2] >> 6)
     const delta = ((source[i + 2] & 0x3f) << 2) | (source[i + 3] >> 8)
     const epsilon = source[i + 3] & 0xff
-    packet.push(alpha, beta, gamma, delta, epsilon)
+    points.push(alpha, beta, gamma, delta, epsilon)
   }
 
-  return trimRight(Uint8Array.from(packet))
+  return trimRight(Uint8Array.from(points))
 }
--- src/hex.ts
+++ src/hex.ts
@@ -1,7 +1,7 @@
 /**
  * Encode Uint8Array to hex string
  */
-export function encode(input: Uint8Array): string {
+export function encode(input: Uint8Array) {
   const reduceFn = (input: string, point: number) => input + point.toString(16).padStart(2, '0')
   return input.reduce(reduceFn, '')
 }
@@ -9,10 +9,10 @@ export function encode(input: Uint8Array): string {
 /**
  * Decode hex string to Uint8Array
  */
-export function decode(input: string): Uint8Array {
-  const inputs = input.match(/.{1,2}/g)
-  if (inputs === null) {
+export function decode(input: string) {
+  const points = input.match(/.{1,2}/g)
+  if (points === null) {
     return new Uint8Array()
   }
-  return Uint8Array.from(inputs.map((point: string) => Number.parseInt(point, 16)))
+  return Uint8Array.from(points.map((point) => Number.parseInt(point, 16)))
 }
