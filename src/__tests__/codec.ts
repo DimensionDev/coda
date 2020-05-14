198556db409c787aa43ebad26f54b7cba7e3e058
Septs <github@septs.pw>
2020-05-14 15:57:56 +0800

    feat: simpilfy tests

--- src/__tests__/codec.ts
+++ src/__tests__/codec.ts
@@ -1,16 +1,17 @@
-import { to, Codec, decode, encode } from '..'
+import { Codec, decode, encode } from '..'
 import { base1024Emojis as EMOJIS } from '../constants'
 
-// "Maskbook" Contants
-const MASKBOOK_HEX = '4d61736b626f6f6b'
-const MASKBOOK_BASE_64 = 'TWFza2Jvb2s='
-const MASKBOOK_UTF_8 = 'Maskbook'
-// prettier-ignore
-const MASKBOOK_BASE_1024 =
-  '\ud83d\udc1f\ud83d\udd02' +
-  '\ud83c\udfc1\ud83e\udd16' +
-  '\ud83d\udca7\ud83d\ude8a' +
-  '\ud83d\ude24\ud83c\udc04'
+const codecs: Record<Codec, string> = {
+  [Codec.UTF8]: 'Maskbook',
+  [Codec.Hexadecimal]: '4d61736b626f6f6b',
+  [Codec.Base64]: 'TWFza2Jvb2s=',
+  // prettier-ignore
+  [Codec.Base1024]: (
+    '\ud83d\udc1f\ud83d\udd02\ud83c\udfc1\ud83e\udd16' +
+    '\ud83d\udca7\ud83d\ude8a\ud83d\ude24\ud83c\udc04'
+  )
+}
+
 const SECRET_AES_WITH_LINK =
   'https://www.microsoft.com/%204/4.-bvXKJyn2SnRTSkdXk9aatF37jDhuOfTrDT/Zt/' +
   '5BRfy0ns2dyneAiWSOFi535LtFdTkwu1mO/ikGDqMZ8XJ2dYwy4whjCbV1lC7QL+hQl4W7w8' +
@@ -19,32 +20,13 @@ const SECRET_AES_WITH_LINK =
   'xQc4C9iYgNWlySXoHarCj+ph6NeaJHyTRU9d8KDU28N359XDUA2sxn82ne/RdG57DYV5+sGD' +
   'Pw==.Auqv1q+v53lJseTRpSgQGQPN2OOOqPRD3rIIJMtqY9+i.0%40'
 
-// Codec basic equal chain
-function eqChain(codec: Uint8Array) {
-  expect(to(codec, Codec.Hex)).toBe(MASKBOOK_HEX)
-  expect(to(codec, Codec.UTF8)).toBe(MASKBOOK_UTF_8)
-  expect(to(codec, Codec.Base64)).toBe(MASKBOOK_BASE_64)
-  expect(to(codec, Codec.Base1024)).toBe(MASKBOOK_BASE_1024)
-}
-
-// Build Codec from hex
-test('build codec from hex', () => {
-  eqChain(to(MASKBOOK_HEX, Codec.Hex))
-})
-
-// Build Codec from utf-8
-test('build codec from utf-8', () => {
-  eqChain(to(MASKBOOK_UTF_8, Codec.UTF8))
-})
-
-// Build Codec from base64
-test('build codec from base64', () => {
-  eqChain(to(MASKBOOK_BASE_64, Codec.Base64))
-})
-
-// Build Codec from base-1024
-test('build codec from base1024', () => {
-  eqChain(to(MASKBOOK_BASE_1024, Codec.Base1024))
+test('build codec', () => {
+  for (const codec of Object.keys(codecs) as Codec[]) {
+    const decoded = decode(codecs[codec], codec)
+    expect(Buffer.from(decoded).toString('utf-8')).toEqual('Maskbook')
+    const encoded = encode(decoded, codec)
+    expect(encoded).toEqual(codecs[codec])
+  }
 })
 
 // Validate the chosen emojis
@@ -55,13 +37,11 @@ test('validate the chosen emojis', () => {
 
 // Real world AES example
 test('real world AES example', () => {
-  const decoded = to(SECRET_AES_WITH_LINK, Codec.UTF8)
-  // check hex
-  expect(areEqual(decode(encode(decoded, Codec.Hex), Codec.Hex), decoded)).toBe(true)
-  // check base64
-  expect(areEqual(decode(encode(decoded, Codec.Base64), Codec.Base64), decoded)).toBe(true)
-  // check base1024
-  expect(areEqual(decode(encode(decoded, Codec.Base1024), Codec.Base1024), decoded)).toBe(true)
+  const decoded = decode(SECRET_AES_WITH_LINK, Codec.UTF8)
+  const codecs = [Codec.Hexadecimal, Codec.UTF8, Codec.Base64, Codec.Base1024]
+  for (const codec of codecs) {
+    expect(areEqual(decode(encode(decoded, codec), codec), decoded)).toBe(true)
+  }
 })
 
 function areEqual(a: Uint8Array, b: Uint8Array) {
--- src/codec.ts
+++ src/codec.ts
@@ -3,91 +3,57 @@ import * as Utf8 from './utf8'
 import * as Base64 from './base64'
 import * as Base1024 from './base1024'
 
-/**
- * Codec
- *
- * Encoding/Decoding string with Unicode code points(0 - 0x10FFFF).
- *
- * All data will be formated into `Uint8Array` while constructing Codec
- * class, for example, Let's format the word `Mask`:
- *
- * For `Base1024`:
- *
- * | Text |   Binary   | Index |
- * | ---- | ---------- | ----- |
- * |      | 0100110101 |  309  |
- * |      | 1000010111 |  535  |
- * |      | 0011011011 |  219  |
- *
- * For `UTF-8`: `[ 01001101, 01100001, 01110011, 01101011 ]`
- *
- * | Text |  Binary  | Index |
- * | ---- | -------- | ----- |
- * |  M   | 01001101 |  77   |
- * |  a   | 01100001 |  97   |
- * |  s   | 01110011 |  115  |
- * |  k   | 01101011 |  107  |
- *
- * For `Base64`: `[ 010011, 010110, 000101, 110011, 011010, 11 ]`
- *
- * | Text | Binary | Index |
- * | ---- | ------ | ----- |
- * |  T   | 010011 |  19   |
- * |  W   | 010110 |  22   |
- * |  F   | 000101 |  5    |
- * |  z   | 110011 |  51   |
- * |  a   | 011010 |  26   |
- * |  s   | 110000 |  48   |
- * |  =   | 000000 |       |
- *
- * For `hex`: `[ 0100, 1101, 0110, 0001, 0111, 0011, 0110, 1011 ]`
- *
- * | Text | Binary | Index |
- * | ---- | ------ | ----- |
- * |  4   |  0100  |   4   |
- * |  d   |  1101  |   13  |
- * |  6   |  0110  |   6   |
- * |  1   |  0001  |   1   |
- * |  7   |  0111  |   7   |
- * |  3   |  0011  |   3   |
- * |  6   |  0110  |   6   |
- * |  b   |  1011  |   11  |
- */
 export enum Codec {
-  Hex = 'Hex',
-  UTF8 = 'UTF8',
-  Base64 = 'Base64',
-  Base1024 = 'Base1024',
-  Buffer = 'Buffer',
-}
-
-/** decode (shorthand) */
-export function to(input: string, inputCodec: Codec): Uint8Array
-/** decode */
-export function to(input: string, inputCodec: Codec, outputCodec: Codec.Buffer): Uint8Array
-/** encode (shorthand) */
-export function to(input: Uint8Array, outputCodec: Codec): string
-/** encode */
-export function to(input: Uint8Array, inputCodec: Codec.Buffer, outputCodec: Codec): string
-/** copy */
-export function to(input: Uint8Array, inputCodec: Codec.Buffer, outputCodec: Codec.Buffer): Uint8Array
-/** decode and encode */
-export function to(input: string, inputCodec: Codec, outputCodec: Codec): string
-export function to(input: any, inputCodec: Codec, outputCodec = Codec.Buffer): any {
-  if (isUint8Array(input)) {
-    if (inputCodec === Codec.Buffer && outputCodec === Codec.Buffer) {
-      return Uint8Array.from(input)
-    } else {
-      return encode(input, outputCodec !== Codec.Buffer ? outputCodec : inputCodec)
-    }
-  } else if (typeof input === 'string') {
-    if (outputCodec === Codec.Buffer) {
-      return decode(input, inputCodec)
-    } else if (inputCodec !== Codec.Buffer) {
-      return encode(decode(input, inputCodec), outputCodec)
-    }
-  }
-  throw new Error(`${Codec[inputCodec]} to ${Codec[outputCodec]} not supported`)
+  /**
+   * Let's format the word `Mask`:
+   *
+   * | Text | Binary | Index |
+   * | ---- | ------ | ----- |
+   * |  4   |  0100  |   4   |
+   * |  d   |  1101  |   13  |
+   * |  6   |  0110  |   6   |
+   * |  1   |  0001  |   1   |
+   * |  7   |  0111  |   7   |
+   * |  3   |  0011  |   3   |
+   * |  6   |  0110  |   6   |
+   * |  b   |  1011  |   11  |
+   **/
+  Hexadecimal = 'hex',
+  /**
+   * Let's format the word `Mask`:
+   *
+   * | Text |  Binary  | Index |
+   * | ---- | -------- | ----- |
+   * |  M   | 01001101 |  77   |
+   * |  a   | 01100001 |  97   |
+   * |  s   | 01110011 |  115  |
+   * |  k   | 01101011 |  107  |
+   **/
+  UTF8 = 'utf8',
+  /**
+   * Let's format the word `Mask`:
+   *
+   * | Text | Binary | Index |
+   * | ---- | ------ | ----- |
+   * |  T   | 010011 |  19   |
+   * |  W   | 010110 |  22   |
+   * |  F   | 000101 |  5    |
+   * |  z   | 110011 |  51   |
+   * |  a   | 011010 |  26   |
+   * |  s   | 110000 |  48   |
+   * |  =   | 000000 |       |
+   **/
+  Base64 = 'base64',
+  /**
+   * Let's format the word `Mask`:
+   *
+   * | Text |   Binary   | Index |
+   * | ---- | ---------- | ----- |
+   * |      | 0100110101 |  309  |
+   * |      | 1000010111 |  535  |
+   * |      | 0011011011 |  219  |
+   **/
+  Base1024 = 'base1024',
 }
 
 export function encode(input: Uint8Array, codec = Codec.Base64) {
@@ -95,7 +61,7 @@ export function encode(input: Uint8Array, codec = Codec.Base64) {
     throw new Error('input type error')
   }
   switch (codec) {
-    case Codec.Hex:
+    case Codec.Hexadecimal:
       return Hex.encode(input)
     case Codec.UTF8:
       return Utf8.encode(input)
@@ -104,7 +70,6 @@ export function encode(input: Uint8Array, codec = Codec.Base64) {
     case Codec.Base1024:
       return Base1024.encode(input)
   }
-  throw new Error(`${Codec[codec]} not supported`)
 }
 
 export function decode(input: string, codec = Codec.Base64) {
@@ -112,7 +77,7 @@ export function decode(input: string, codec = Codec.Base64) {
     throw new Error('input type error')
   }
   switch (codec) {
-    case Codec.Hex:
+    case Codec.Hexadecimal:
       return Hex.decode(input)
     case Codec.UTF8:
       return Utf8.decode(input)
@@ -121,7 +86,6 @@ export function decode(input: string, codec = Codec.Base64) {
     case Codec.Base1024:
       return Base1024.decode(input)
   }
-  throw new Error(`${Codec[codec]} not supported`)
 }
 
 function isUint8Array(input: any): input is Uint8Array {
