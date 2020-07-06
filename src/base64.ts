8f0376a58eb273cfd55d222c1bb817080ca27e1f
Septs <github@septs.pw>
2020-07-06 11:03:59 +0800

    fix: base64 behavior

--- src/base64.ts
+++ src/base64.ts
@@ -2,22 +2,12 @@
  * Encode Uint8Array to base64
  */
 export function encode(input: Uint8Array): string {
-  const decoded = new TextDecoder().decode(input)
-  if (typeof btoa !== 'undefined') {
-    return btoa(decoded)
-  }
-  return Buffer.from(decoded, 'utf-8').toString('base64')
+  return Buffer.from(input).toString('base64')
 }
 
 /**
  * Decode base64 to Uint8Array
  */
 export function decode(input: string): Uint8Array {
-  let decoded = ''
-  if (typeof atob !== 'undefined') {
-    decoded = atob(input)
-  } else {
-    decoded = Buffer.from(input, 'utf-8').toString('base64')
-  }
-  return new TextEncoder().encode(decoded)
+  return Buffer.from(input, 'base64')
 }
