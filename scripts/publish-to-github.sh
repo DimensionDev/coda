81c3169b52a4ed37cf3e4964528472f3979713d9
Septs <github@septs.pw>
2020-07-18 13:24:10 +0800

    chore: fix npm-scripts

--- scripts/publish-to-github.sh
+++ scripts/publish-to-github.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+npm config set @dimensiondev:registry https://npm.pkg.github.com
 COMMIT_HSAH=$(git rev-parse --short HEAD)
 npm --no-git-tag-version version "0.0.0-$COMMIT_HSAH"
-npm publish --tag unstable --registry https://npm.pkg.github.com/
+npm publish --tag unstable
