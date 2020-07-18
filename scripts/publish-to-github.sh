a2a48b7a9df496f1906191b891874a1e7c12602a
Septs <github@septs.pw>
2020-07-18 13:42:49 +0800

    chore: fix npm-scripts

--- scripts/publish-to-github.sh
+++ scripts/publish-to-github.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-cat >~/.npmrc <<EOL
-@dimensiondev:registry=https://npm.pkg.github.com
-//npm.pkg.github.com/:_authToken=${NPM_AUTH_TOKEN}
-EOL
+echo "@dimensiondev:registry=https://npm.pkg.github.com" > "$HOME/.npmrc"
+echo "//npm.pkg.github.com/:_authToken=${NPM_AUTH_TOKEN}" >> "$HOME/.npmrc"
 
 COMMIT_HSAH=$(git rev-parse --short HEAD)
 npm --no-git-tag-version version "0.0.0-$COMMIT_HSAH"
