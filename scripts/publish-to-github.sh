ca2cd82323bb13aa4ce59a87334ff3cb7ea05e7f
Septs <github@septs.pw>
2020-07-18 13:37:09 +0800

    chore: fix npm-scripts

--- scripts/publish-to-github.sh
+++ scripts/publish-to-github.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-npm config set @dimensiondev:registry https://npm.pkg.github.com/DimensionDev
+cat >~/.npmrc <<EOL
+@dimensiondev:registry=https://npm.pkg.github.com
+//npm.pkg.github.com/:_authToken=${NPM_AUTH_TOKEN}
+EOL
+
 COMMIT_HSAH=$(git rev-parse --short HEAD)
 npm --no-git-tag-version version "0.0.0-$COMMIT_HSAH"
 npm publish --tag unstable
